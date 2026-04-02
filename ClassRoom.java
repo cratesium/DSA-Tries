e9ca324d9dba3fbff1de1f451bfe99f86ac8400e
Abhii <99896359+cratesium@users.noreply.github.com>
2026-04-02 23:50:08 +0530

    Add search method to ClassRoom class

--- ClassRoom.java
+++ ClassRoom.java
@@ -48,6 +48,18 @@ public class ClassRoom {
 
 
     }
+       // SEARCH
+    public boolean search(String word) {
+        Node curr = root;
+        for (int i = 0; i < word.length(); i++) {
+            int idx = word.charAt(i) - 'a';
+            if (curr.children[idx] == null)
+                return false;
+            curr = curr.children[idx];
+        }
+        return curr.isEndOfWord;
+    }
+    
     
         // INSERT
     public void insert(String word) {
